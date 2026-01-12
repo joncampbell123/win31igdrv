49bb86107744636c34486ace8096643bda36483e
Jonathan Campbell <jonathan@castus.tv>
2026-01-11 19:58:57 -0800

    Finish conversion

--- dosbga31/286/display/1plane/cursor.asm
+++ dosbga31/286/display/1plane/cursor.asm
@@ -177,8 +177,8 @@ sBegin	Data
 	public	CUR_ROUND_RIGHT
 	public	save_area
 	public	screen_buf
-	public	X_SCAN_INC
-	public	X_SCAN_BYTES
+	public	SCAN_INC
+	public	SCAN_BYTES
 
 ;	cur_cursor contains the cursor data structure (less the
 ;	actual bits) for the current cursor shape.
@@ -266,8 +266,8 @@ screen_buf	dw	0
 
 ;
 
-X_SCAN_INC	dw	0
-X_SCAN_BYTES	dw	0
+SCAN_INC	dw	0
+SCAN_BYTES	dw	0
 
 ;	The following are the masks which make up the cursor image.
 
@@ -517,7 +517,7 @@ copy_save_to_screen proc near
 	call	compute_screen_pointer	;Compute address on screen
 	xchg	si,di
 	mov	cx,SAVE_WIDTH		;Get width of save area
-	mov	ax,X_SCAN_BYTES		;Get axis update values
+	mov	ax,SCAN_BYTES		;Get axis update values
 	sub	ax,cx			;SCAN_BYTES-SAVE_WIDTH (ax -= cx)
 	mov	bp,CUR_HEIGHT		;Set maximum to move (entire save buf)
 	jmp	buf_to_screen_10
@@ -1068,7 +1068,7 @@ map_xy	endp
 
 copy_buffer_to_screen proc near
 
-	mov	ax,X_SCAN_INC		;Prepare for the copy
+	mov	ax,SCAN_INC		;Prepare for the copy
 	mov	si,screen_buf		;--> source
 	mov	di,screen_pointer	;--> destination
 	mov	cx,BUF_WIDTH		;Get width of the buffer
@@ -1334,7 +1334,7 @@ erase_old_cursor endp
 
 copy_screen_to_buffer proc near
 
-	mov	ax,X_SCAN_INC		;Prepare for the copy
+	mov	ax,SCAN_INC		;Prepare for the copy
 	mov	cx,buf_height		;Set # scans to copy (round up)
 	inc	cx
 	sar	cx,1
--- dosbga31/286/display/1plane/egainit.asm
+++ dosbga31/286/display/1plane/egainit.asm
@@ -39,8 +39,8 @@ incDevice = 1				;allow assembly of needed constants
 	externW		real_y
 	externW		save_area
 	externW		screen_buf
-	externW		X_SCAN_INC
-	externW		X_SCAN_BYTES
+	externW		SCAN_INC
+	externW		SCAN_BYTES
 
 sBegin	Code
 
@@ -160,11 +160,11 @@ cBegin
 
 	mov	ax,SCREEN_W_BYTES
 	mov	es:CODE_SCREEN_W_BYTES,ax
-	mov	X_SCAN_BYTES,ax
+	mov	SCAN_BYTES,ax
 
 	mov	bx,SCREEN_W_BYTES
 	sub	bx,BUF_WIDTH
-	mov	X_SCAN_INC,bx
+	mov	SCAN_INC,bx
 
 	; done with CODE seg
 	mov	bx,es
